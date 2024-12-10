9cf82bb5b54a19a712f670f47956d6f74f0f7d20
Benjamin Caron <caron518@gmail.com>
2024-12-10 15:27:56 -0500

    Create Task.js

---
+++ backend/models/Task.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const TaskSchema = new mongoose.Schema({
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+    },
+    dueDate: {
+        type: Date,
+    },
+    status: {
+        type: String,
+        enum: ['pending', 'completed'],
+        default: 'pending',
+    },
+});
+
+module.exports = mongoose.model('Task', TaskSchema);
